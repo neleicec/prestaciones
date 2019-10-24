18cdc94ef9bae52dff8e21f692c23ea55896b4af
Neleicec <neleicec@gmail.com>
2019-10-24 12:33:26 -0400

    Creando Nuevos Campos

--- models/prest.py
+++ models/prest.py
@@ -7,7 +7,14 @@ class prest(models.Model):
 	name = fields.Many2one('hr.employee', string='Employee', required=True)
 	trimestre = fields.Selection(string = 'Selection Trimester', selection=[('trimestre1', 'First Trimester'),('trimestre2','Second Trimester'),('trimestre3','Third Quarter'),('trimestre4','Fourth Trimester')],required=True, help='Indique el trimestre a calcular')
 	wage1 = fields.Float('Wage',digits=(16,2), required=True, related='name.wage1',readonly=True, help="Basic Salary of the employee")
-	wage_day1= fields.Float(string='Salario Diario', digits=(26,2),)
+#Pendiente por calcular
+	wage_day1= fields.Float(string='Salario Diario', digits=(26,2),) 
+	alic_util= fields.Float(string='Alicuota de Utilidades', digits=(26,2))
+	alic_vac= fields.Float(string='Alicuota de Vacaciones', digits=(26,2))
+	sal_int= fields.Float(string='Salario Integral', digits=(26,2))
+# #Son fijos
+# 	def dias(self):
+# 	return 15
 
 class employee(models.Model):
 	_inherit='hr.employee'
