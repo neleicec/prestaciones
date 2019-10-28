bf5ae0d5362953aebbb5950af2f5b1abbded8bfa
Neleicec <neleicec@gmail.com>
2019-10-28 17:18:35 -0400

    Cálculo de Salario Diario y Alicuota de Utilidades

--- models/prest.py
+++ models/prest.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*- 
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 class prest(models.Model):
 	_name='prest'
@@ -8,16 +8,25 @@ class prest(models.Model):
 	trimestre = fields.Selection(string = 'Selection Trimester', selection=[('trimestre1', 'First Trimester'),('trimestre2','Second Trimester'),('trimestre3','Third Quarter'),('trimestre4','Fourth Trimester')],required=True, help='Indique el trimestre a calcular')
 	wage1 = fields.Float('Wage',digits=(16,2), required=True, related='name.wage1',readonly=True, help="Basic Salary of the employee")
 #Pendiente por calcular
-	wage_day1= fields.Float(string='Salario Diario', digits=(26,2),) 
-	alic_util= fields.Float(string='Alicuota de Utilidades', digits=(26,2))
 	alic_vac= fields.Float(string='Alicuota de Vacaciones', digits=(26,2))
 	sal_int= fields.Float(string='Salario Integral', digits=(26,2))
 	
-# #Son fijos
+#Son fijos
  	def _dias(self):
 		return 15
-	dias_sal = fields.Integer(string='Dias', default=_dias)
+	dias_sal = fields.Integer(string='Dias', default=_dias, readonly=True)
 
+	@api.onchange('name')
+	def _saldia(self):
+		for record in self:
+			record.wage_day1 = record.wage1 / 30
+	wage_day1= fields.Float(string='Salario Diario', digits=(26,2), default=_saldia, readonly=True)
+
+	@api.onchange('name')
+	def _alic(self):
+		for record in self:
+			record.alic_util = (((record.wage1/30) * 30)/360)
+	alic_util= fields.Float(string='Alicuota de Utilidades', digits=(26,2),default=_alic)
 # Crar campo Wage1 en Empleado
 class employee(models.Model):
 	_inherit='hr.employee'
