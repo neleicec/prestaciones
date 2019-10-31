e1c8dcf13665cf9dfa9da44c209b242cf1ef0a24
Neleicec <neleicec@gmail.com>
2019-10-31 13:19:11 -0400

    Commit

--- models/prest.py
+++ models/prest.py
@@ -8,7 +8,7 @@ class prest(models.Model):
 	trimestre = fields.Selection(string = 'Selection Trimester', selection=[('trimestre1', 'First Trimester'),('trimestre2','Second Trimester'),('trimestre3','Third Quarter'),('trimestre4','Fourth Trimester')],required=True, help='Indique el trimestre a calcular')
 	wage1 = fields.Float('Wage',digits=(16,2), required=True, related='name.wage1',readonly=True, help="Basic Salary of the employee")
 #Pendiente por calcular
-	alic_vac= fields.Float(string='Alicuota de Vacaciones', digits=(26,2))
+	
 	sal_int= fields.Float(string='Salario Integral', digits=(26,2))
 	
 #Son fijos
@@ -27,7 +27,17 @@ class prest(models.Model):
 		for record in self:
 			record.alic_util = (((record.wage1/30) * 30)/360)
 	alic_util= fields.Float(string='Alicuota de Utilidades', digits=(26,2),default=_alic, readonly=True)
+
+	@api.onchange('name')
+	def _vac(self):
+		for record in self:
+			record.alic_vac = record.vac_concepto +10
+	alic_vac= fields.Float(string='Alicuota de Vacaciones', digits=(26,2), compute=_vac)
+
+
+	vac_concepto= fields.Integer(string='Concepto Vacaciones', required=True, related='name.concepto_vac', readonly=True)
 # Crar campo Wage1 en Empleado
 class employee(models.Model):
 	_inherit='hr.employee'
 	wage1 = fields.Float('Wage', digits=(16,2), required=True)
+	concepto_vac = fields.Integer(string='Concepto Vacaciones', required=True, help="Número de días que le pagan al trabajador por concepto de vacaciones")
