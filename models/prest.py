3caf103ccb421402270d868b90536bf4751870e6
Neleicec <neleicec@gmail.com>
2019-11-01 10:46:17 -0400

    Días Adicionales

--- models/prest.py
+++ models/prest.py
@@ -4,52 +4,97 @@ from odoo import models, fields, api
 
 class prest(models.Model):
 	_name='prest'
-	name = fields.Many2one('hr.employee', string='Employee', required=True)
-	trimestre = fields.Selection(string = 'Selection Trimester', selection=[('trimestre1', 'First Trimester'),('trimestre2','Second Trimester'),('trimestre3','Third Quarter'),('trimestre4','Fourth Trimester')],required=True, help='Indique el trimestre a calcular')
-	wage1 = fields.Float('Wage',digits=(16,2), required=True, related='name.wage1',readonly=True, help="Basic Salary of the employee")
-#Pendiente por calcular
-	vac_concepto= fields.Float(string='Concepto Vacaciones', required=True, related='name.concepto_vac', readonly=True)
+	name = fields.Many2one('hr.employee', 
+		string='Employee', 
+		required=True)
+	trimestre = fields.Selection(
+		string = 'Selection Trimester', 
+		selection=[('trimestre1', 'First Trimester'),('trimestre2','Second Trimester'),('trimestre3','Third Quarter'),('trimestre4','Fourth Trimester')],
+		required=True, 
+		help='Indique el trimestre a calcular')
+	wage1 = fields.Float('Wage',
+		digits=(16,2), 
+		required=True, 
+		related='name.wage1',
+		readonly=True, 
+		help="Basic Salary of the employee")
+	vac_concepto= fields.Float(
+		string='Concepto Vacaciones', 
+		required=True, 
+		related='name.concepto_vac', 
+		readonly=True)
 	
 	
-#Son fijos
  	def _dias(self):
 		return 15
-	dias_sal = fields.Integer(string='Dias', default=_dias, readonly=True)
+	dias_sal = fields.Integer(
+		string='Dias', 
+		default=_dias, 
+		readonly=True)
 
 	@api.onchange('name')
 	def _saldia(self):
 		for record in self:
 			record.wage_day1 = record.wage1 / 30
-	wage_day1= fields.Float(string='Salario Diario', digits=(26,2), default=_saldia, readonly=True)
+	wage_day1= fields.Float(
+		string='Salario Diario', 
+		digits=(26,2), 
+		default=_saldia, 
+		readonly=True)
 
 	@api.onchange('name')
 	def _alic(self):
 		for record in self:
 			record.alic_util = (((record.wage1/30) * 30)/360)
-	alic_util= fields.Float(string='Alicuota de Utilidades', digits=(26,2),default=_alic, readonly=True)
+	alic_util= fields.Float(
+		string='Alicuota de Utilidades', 
+		digits=(26,2),
+		default=_alic, 
+		readonly=True)
 
 	@api.onchange('name')
 	def _vac(self):
 		for record in self:
 			record.alic_vac = ((record.wage1/30)*(record.vac_concepto) / 360)
-	alic_vac= fields.Float(string='Alicuota de Vacaciones', digits=(26,2), compute=_vac)
+	alic_vac= fields.Float(
+		string='Alicuota de Vacaciones', 
+		digits=(26,2), 
+		compute=_vac)
 
 	@api.onchange('name')
 	def _int(self):
 		for record in self:
 			record.sal_int = ((record.wage1/30)+ record.alic_util + record.vac_concepto)
-	sal_int= fields.Float(string='Salario Integral', digits=(26,2), readonly=True)
+	sal_int= fields.Float(
+		string='Salario Integral', 
+		digits=(26,2), 
+		readonly=True)
 
-	# @api.onchange('name')
-	# def _dias(self):
-	# 	if self.years_service >= 2:
-	# 		self.dias_adi=self.dias_adi + 2
-	# dias_adi= fields.Float(string='Días adicionales', relate='name.dias_sal', default=_dias)		
+
+	@api.onchange('name')
+	def _diah(self):
+		for record in self:
+			if (record.name.years_service) <= 2.0:
+				dias_h = 0.0
+			else: 
+				years_service2 = float(record.name.years_service)
+				sumador = years_service2 - 1.0
+				sumadorx = sumador * 2.0 
+				record.dias_h = sumadorx
+	dias_h= fields.Float(
+		string='Dias Adicionales', 
+		digits=(26,2), 
+		readonly=True, 
+		default=_diah)
+
 # Crar campo Wage1 en Empleado
 
 class employee(models.Model):
 	_inherit='hr.employee'
-	wage1 = fields.Float('Wage', digits=(16,2), required=True)
-	concepto_vac = fields.Float(string='Concepto Vacaciones', required=True, help="Número de días que le pagan al trabajador por concepto de vacaciones")
-
-	anos=fields.Float(string='Años de Servicio',invisible=True)
+	wage1 = fields.Float('Wage', 
+		digits=(16,2), 
+		required=True)
+	concepto_vac = fields.Float(
+		string='Concepto Vacaciones', 
+		required=True, 
+		help="Número de días que le pagan al trabajador por concepto de vacaciones")
